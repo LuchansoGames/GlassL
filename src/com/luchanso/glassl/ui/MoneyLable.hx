89cb90881ea06227df2b32799d4cf0f688066577
luchanso <oleg@loutchansky.com>
2016-04-02 16:24:51 +0300

    Add moneyLable

---
+++ src/com/luchanso/glassl/ui/MoneyLable.hx
@@ -0,0 +1,38 @@
+package com.luchanso.glassl.ui;
+
+import flash.text.TextField;
+import openfl.text.TextFieldAutoSize;
+import openfl.text.TextFormat;
+
+/**
+ * ...
+ * @author Loutchansky Oleg
+ */
+class MoneyLable extends TextField
+{
+	var _money = 0;
+	
+	public var money(get, set) : Int;
+
+	public function new(color : Int = 0xFFFFFF)
+	{
+		super();
+		
+		this.defaultTextFormat = new TextFormat("Arial", 18, color);
+		this.autoSize = TextFieldAutoSize.LEFT;
+		this.mouseEnabled = false;
+		this.selectable = true;
+		this.text = "0 монеток";
+	}
+	
+	function get_money():Int
+	{
+		return _money;
+	}
+	
+	function set_money(value:Int):Int 
+	{
+		return _money = value;
+	}
+	
+}
