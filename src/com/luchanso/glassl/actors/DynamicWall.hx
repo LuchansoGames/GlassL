adab1a6ad8f3bcb7e8e34eaf65fdabb0d2db17fc
luchanso <oleg@loutchansky.com>
2016-04-02 16:20:51 +0300

    Add Dotted Line

--- src/com/luchanso/glassl/actors/DynamicWall.hx
+++ src/com/luchanso/glassl/actors/DynamicWall.hx
@@ -13,15 +13,19 @@ class DynamicWall extends Wall
 {
 	public var isActive : Bool = false;
 	public var isSleep : Bool = false;
+	public var wall : Wall;
 
 	public function new(x, y, width, height) 
 	{
 		super(x, y, width, height);
-		this.alpha = 0;
+		
+		wall = new Wall(0, 0, width, height);
+		wall.alpha = 0;
+		this.addChild(wall);
 	}
 	
 	private override function draw(width : Float, height : Float) : Void
-	{
+	{	
 		var countSprips = 10;
 		var x : Float = 0;
 		var y : Float = 0;
@@ -31,7 +35,7 @@ class DynamicWall extends Wall
 		{
 			x = tempWidth + tempWidth * 2 * i;
 			
-			graphics.beginFill(Config.colorLight);
+			graphics.beginFill(Config.colorLight, 0.4);
 			graphics.drawRect(x, y, tempWidth, height);
 			graphics.endFill();
 		}
@@ -42,17 +46,17 @@ class DynamicWall extends Wall
 		this.isActive = true;
 		this.isSleep = true;
 		
-		Actuate.tween(this, time / 2, { alpha: 1 } ).ease(Linear.easeNone).onComplete(function() {
+		Actuate.tween(this.wall, time / 2, { alpha: 1 } ).ease(Linear.easeNone).onComplete(function() {
 			hide(time / 2);
 		});
-		Actuate.tween(this, time * 1.5, { } ).onComplete(function() {
+		Actuate.tween(this.wall, time * 1.5, { } ).onComplete(function() {
 			this.isSleep = false;
 		});
 	}
 	
 	public function hide(time : Float) : Void
 	{
-		Actuate.tween(this, time, { alpha: 0 } ).ease(Linear.easeNone).onComplete(function() {
+		Actuate.tween(this.wall, time, { alpha: 0 } ).ease(Linear.easeNone).onComplete(function() {
 			this.isActive = false;
 		});
 	}
