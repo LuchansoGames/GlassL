88d2dd9121be09de78de4dbb755b19d7f701bc1e
luchanso <oleg@loutchansky.com>
2016-04-02 16:21:14 +0300

    Add animation effect to wall

--- src/com/luchanso/glassl/actors/Wall.hx
+++ src/com/luchanso/glassl/actors/Wall.hx
@@ -1,6 +1,8 @@
 package com.luchanso.glassl.actors;
 
 import com.luchanso.glassl.Config;
+import motion.Actuate;
+import motion.easing.Linear;
 import openfl.display.Sprite;
 import openfl.filters.DropShadowFilter;
 
@@ -10,6 +12,10 @@ import openfl.filters.DropShadowFilter;
  */
 class Wall extends Sprite
 {
+	private var _color : Int = 0;
+	private var color(get, set) : Int;
+	private var animationComplete : Bool = true;
+	
 	public function new(x, y, width, height) 
 	{
 		super();
@@ -17,13 +23,40 @@ class Wall extends Sprite
 		this.x = x;
 		this.y = y;
 		
+		_color = Config.colorLight;
+		
 		draw(width, height);
+	}
+	
+	public function bounce()
+	{
+		if (animationComplete)
+		{
+			Actuate.transform(this, 0.25).color(Config.colorBounce).ease(Linear.easeNone).reflect().reverse().onComplete(function() { 
+				animationComplete = true;
+			} );
+		}
 	}
 	
 	private function draw(width : Float, height : Float) : Void
 	{
-		graphics.beginFill(Config.colorLight);
+		graphics.clear();
+		graphics.beginFill(color);
 		graphics.drawRect(0, 0, width, height);
 		graphics.endFill();
+	}
+	
+	function get_color():Int 
+	{
+		return _color;
+	}
+	
+	function set_color(value:Int):Int 
+	{
+		_color = value;
+		
+		draw(this.width, this.height);
+		
+		return _color;
 	}
 }
