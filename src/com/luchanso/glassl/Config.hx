e44cd6e85ee5aa84744c47f20c166ff9c5c2c305
luchanso <oleg@loutchansky.com>
2016-04-02 16:19:35 +0300

    Remove trash from co

--- src/com/luchanso/glassl/Config.hx
+++ src/com/luchanso/glassl/Config.hx
@@ -6,11 +6,9 @@ package com.luchanso.glassl;
  */
 class Config
 {
-	public static var colorDark : Int = 0x3F51B5;
+	public static var colorBounce : Int = 0xFFFF00;
 	public static var colorLight : Int = 0x03A9F4;
 	public static var colorScore : Int = 0xAFE7FE;
-	public static var width : Float = 600;
-	public static var height : Float = 700;
 	
 	public static var soundOn : Bool = true;
 }
