6a81ee74ed1627b1f980c8ded5e18e482155f65b
luchanso <oleg@loutchansky.com>
2016-04-19 14:10:37 +0300

    Add config secret and ip server

--- src/com/luchanso/glassl/Config.hx
+++ src/com/luchanso/glassl/Config.hx
@@ -11,4 +11,7 @@ class Config
 	public static var colorScore : Int = 0xAFE7FE;
 	
 	public static var soundOn : Bool = true;
+	
+	public static var secret : String = "$1$cpwdDRMK$P7z564nKedEutjtSISisp1";
+	public static var urlAddress : String = "http://37.139.30.134";
 }
