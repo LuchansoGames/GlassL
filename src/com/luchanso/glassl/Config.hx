3ab7bfb6d77bb59fea0582bcf715b5dd1d5b587d
luchanso <oleg@loutchansky.com>
2016-04-21 14:01:30 +0300

    Change callback to Event in COnfig

--- src/com/luchanso/glassl/Config.hx
+++ src/com/luchanso/glassl/Config.hx
@@ -1,4 +1,6 @@
 package com.luchanso.glassl;
+import flash.events.EventDispatcher;
+import openfl.events.Event;
 
 /**
  * ...
@@ -6,6 +8,9 @@ package com.luchanso.glassl;
  */
 class Config
 {
+	public static var COINS_CHANGE = 'set_coins';
+	public static var events : EventDispatcher = new EventDispatcher();
+	
 	public static var colorBounce : Int = 0xFFFF00;
 	public static var colorLight : Int = 0x03A9F4;
 	public static var colorScore : Int = 0xAFE7FE;
@@ -14,5 +19,21 @@ class Config
 	
 	public static var secret : String = "$1$cpwdDRMK$P7z564nKedEutjtSISisp1";
 	public static var urlAddress : String = "http://37.139.30.134";
-	public static var coins : Int = 0;
+	
+	public static var coins(get, set) : Int;
+	private static var _coins : Int = 0;
+	
+	static function get_coins():Int 
+	{
+		return _coins;
+	}
+	
+	static function set_coins(value:Int):Int 
+	{
+		_coins = value;
+		
+		events.dispatchEvent(new Event(COINS_CHANGE));
+		
+		return _coins;
+	}
 }
